e89d789b7186e5d8babd04cef419791df7e732ee
Prafful33Tak <tak.2@iitj.ac.in>
2024-08-20 09:59:41 +0530

    Add github config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,14 +13,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://orangewoodsim.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/orangewood_devlopers_docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Orangewood', // Usually your GitHub org/user name.
+
+  organizationName: 'prafful-tak', // Usually your GitHub org/user name.
   projectName: 'orangewood_devlopers_docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
